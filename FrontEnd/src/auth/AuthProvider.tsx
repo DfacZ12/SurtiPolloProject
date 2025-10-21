cef6869df78be9035a115870ff21ebae73af45c7
Daniel Felipe <daniel198fam@gmail.com>
2025-10-21 12:56:50 -0500

    Cambios Carpetas Proyecto, separación front y back en el mismo repo.

---
+++ FrontEnd/src/auth/AuthProvider.tsx
@@ -0,0 +1,18 @@
+import { useContext, createContext, useState, useEffect } from "react";
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+const AuthContext = createContext({
+  isAuth: false,
+});
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [isAuth, setIsAuth] = useState(false);
+  return (
+    <AuthContext.Provider value={{ isAuth }}>{children}</AuthContext.Provider>
+  );
+}
+
+export const useAuth = ()=> useContext(AuthContext);//hooks para usar el contexto...
---
+++ FrontEnd/src/main.tsx
@@ -0,0 +1,33 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import ProtectedRoute from "./pages/ProtectedRoute.tsx";
+import { AuthProvider } from "./auth/AuthProvider.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/Home",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
+);
